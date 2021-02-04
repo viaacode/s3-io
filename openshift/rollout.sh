29a4090a3964e03d183dd20817bf00dbb25ffa5c
violetina <schizoid69@gmail.com>
2021-02-04 11:34:35 +0100

    speedup rollout

--- openshift/rollout.sh
+++ openshift/rollout.sh
@@ -4,14 +4,15 @@
 ## $3 tag eg v1.1
 ## $4 dc back
 ## $5 dc front
-login_oc.sh https://do-prd-okp-m0.do.viaa.be:8443/ 1>/dev/null
-oc project s3-components
+#login_oc.sh https://do-prd-okp-m0.do.viaa.be:8443/ 1>/dev/null
+oc project s3-components || exit 1
 echo ..... Rolling out "${2}" to version "${3}" ..... 
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
   oc rollout cancel dc/"${4}" &oc rollout cancel dc/"${5}" &
   oc tag "${1}":"${3}" "${1}":"${2}" || exit 1
   oc rollout latest dc/"${4}" &  oc rollout latest dc/"${5}" &&
-  oc rollout status dc/"${4}"
+#  oc rollout status dc/"${4}"
+  echo Done
 
 wait
