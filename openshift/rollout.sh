17cd49a52628b1008a7d907c189f1692a9be712e
violetina <schizoid69@gmail.com>
2021-02-04 11:41:44 +0100

    speedup rollout

--- openshift/rollout.sh
+++ openshift/rollout.sh
@@ -7,12 +7,9 @@
 #login_oc.sh https://do-prd-okp-m0.do.viaa.be:8443/ 1>/dev/null
 oc project s3-components || exit 1
 echo ..... Rolling out "${2}" to version "${3}" ..... 
-trap "exit" INT TERM ERR
-trap "kill 0" EXIT
   oc rollout cancel dc/"${4}" &oc rollout cancel dc/"${5}" &
   oc tag "${1}":"${3}" "${1}":"${2}" || exit 1
   oc rollout latest dc/"${4}" &  oc rollout latest dc/"${5}" &&
 #  oc rollout status dc/"${4}"
-  echo Done
+echo Done
 
-wait
